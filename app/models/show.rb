474aacaf269e4f985bf57cd4cc923c1aa9b4d41b
Katie Blizzard <kblizzard11@gmail.com>
2019-05-13 00:01:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,11 @@
 class Show < ActiveRecord::Base
   
   def highest_rating 
-    
+    Show.maximum(:rating)
   end 
   
   def most_popular_show 
-    
+    Show.find_by(:rating => highest_rating)
   end 
   
   def lowest_rating
