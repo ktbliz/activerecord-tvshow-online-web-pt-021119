40e7b16de199c97ed54287ec9f5a27f411c4b7c7
Katie Blizzard <kblizzard11@gmail.com>
2019-05-13 00:03:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,23 +9,23 @@ class Show < ActiveRecord::Base
   end 
   
   def lowest_rating
-    
+    Show.minimum(:rating)
   end 
   
   def least_popular_show
-    
+    Show.find_by(:rating => lowest_rating)
   end 
   
   def ratings_sum
-    
+    Show.sum(:rating)
   end 
   
   def popular_shows 
-    
+    Show.where('rating > ?', 5)
   end 
   
   def shows_by_alphabetical_order 
-    
+    Show.order(:name ASC)
   end 
   
   
